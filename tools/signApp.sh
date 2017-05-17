f963fdff967f612aa43d93f57943c14e532156b4
jay-to-the-dee <jay-to-the-dee@users.noreply.github.com>
2017-05-17 10:44:07 +0100

    Fix various things to allow successful build and release compile

--- tools/signApp.sh
+++ tools/signApp.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-cd ..
+#cd ..
 
+rm app-release-aligned.apk
 zipalign -v -p 4 app-release-unsigned.apk app-release-aligned.apk
 apksigner sign --ks my-release-key.jks --out app-release.apk app-release-aligned.apk
 
