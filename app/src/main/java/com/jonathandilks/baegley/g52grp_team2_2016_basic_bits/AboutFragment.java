16d8e7f0e1974d04c3f9d80f1448026c7ce485a1
Enhao Sun <s710412717@163.com>
2017-05-17 10:27:35 +0100

    update AboutFragment
    add description and copyRight

--- app/src/main/java/com/jonathandilks/baegley/g52grp_team2_2016_basic_bits/AboutFragment.java
+++ app/src/main/java/com/jonathandilks/baegley/g52grp_team2_2016_basic_bits/AboutFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.jonathandilks.baegley.g52grp_team2_2016_basic_bits.R;
 
@@ -14,10 +16,30 @@ import com.jonathandilks.baegley.g52grp_team2_2016_basic_bits.R;
  */
 
 public class AboutFragment extends Fragment {
+    private TextView about;
+    private TextView copyRight;
+    private ImageView icon;
+
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        return inflater.inflate(R.layout.aboutfragment, container, false);
+        View rootView = inflater.inflate(R.layout.aboutfragment, container, false);
+
+        about = (TextView) rootView.findViewById(R.id.about);
+        copyRight = (TextView) rootView.findViewById(R.id.copyright);
+        icon = (ImageView) rootView.findViewById(R.id.about_icon);
+
+        String aboutText = "The mobile application acts as an information directory for the University of Nottingham." +
+                "The app allows the users to find out any pertinent information they might need.";
+
+        about.setText(aboutText);
+        String copyRightText = "Copyright 2017, BasicBits\n All rights reserved";
+        copyRight.setText(copyRightText);
+
+        icon.setImageResource(R.mipmap.uonicon);
+        return rootView;
     }
+
+
 }
